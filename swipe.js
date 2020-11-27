2e46a350a1f6d5b96d91f36e7d6b3ed617de0bff
selleronom <3093210+selleronom@users.noreply.github.com>
2020-11-27 22:52:43 +0100

    simplified the logic of horizontal scrolling sensitivy

--- swipe.js
+++ swipe.js
@@ -1,22 +1,12 @@
 var lastSwipe = 0;
-var sensitivity = 25;
-var counterLeft = 0;
-var counterRight = 0;
 
 window.addEventListener("wheel", event => {
-    const delta = Math.sign(event.deltaX);
-    if (Date.now() - lastSwipe > 3000 && delta < 0) {
-        if (counterRight > sensitivity) {
-            window.history.back();
-            lastSwipe = Date.now();
-        }
-        counterRight++;
+    if (Date.now() - lastSwipe > 3000 && event.deltaX < -2) {
+        window.history.back();
+        lastSwipe = Date.now();
     }
-    if (Date.now() - lastSwipe > 3000 && delta > 0) {
-        if (counterLeft > sensitivity) {
-            window.history.forward();
-            lastSwipe = Date.now();
-        }
-        counterLeft++;
+    if (Date.now() - lastSwipe > 3000 && event.deltaX > 2) {
+        window.history.forward();
+        lastSwipe = Date.now();
     }
 });
