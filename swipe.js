11166e1f72bbd19325c38ee211cc8761d394bcf4
selleronom <3093210+selleronom@users.noreply.github.com>
2020-11-27 21:13:10 +0100

    added horizontal counter with sensitivity of 25

--- swipe.js
+++ swipe.js
@@ -1,13 +1,22 @@
 var lastSwipe = 0;
+var sensitivity = 25;
+var counterLeft = 0;
+var counterRight = 0;
+
 window.addEventListener("wheel", event => {
     const delta = Math.sign(event.deltaX);
-
     if (Date.now() - lastSwipe > 3000 && delta < 0) {
-        window.history.back();
-        lastSwipe = Date.now();
+        if (counterRight > sensitivity) {
+            window.history.back();
+            lastSwipe = Date.now();
+        }
+        counterRight++;
     }
     if (Date.now() - lastSwipe > 3000 && delta > 0) {
-        window.history.forward();
-        lastSwipe = Date.now();
+        if (counterLeft > sensitivity) {
+            window.history.forward();
+            lastSwipe = Date.now();
+        }
+        counterLeft++;
     }
 });
