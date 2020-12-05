5c2ec0b3a16b5153a658fd1755699e942417ac45
selleronom <3093210+selleronom@users.noreply.github.com>
2020-12-05 23:06:09 +0100

    Added sensitivity adjustment

---
+++ background-script.js
@@ -0,0 +1,18 @@
+/*
+Initialize default settings using browser.storage.local.
+*/
+const gettingItems = browser.storage.local.get();
+gettingItems.then(onGot, onError);
+
+function onGot(item) {
+    if (item.default_values_initialized != true) {
+        browser.storage.local.set({
+            sensitivity: "3",
+            default_values_initialized: true
+        });
+    }
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
